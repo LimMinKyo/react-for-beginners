67c7b1d97a581411360d611d5c52461126c61d6d
LimMinKyo <story1400@naver.com>
2021-12-20 18:13:47 +0900

    Update Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,5 @@
 import {useParams} from "react-router-dom";
-import { useEffect, useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 function Detail(){
   const {id} = useParams();
