2b21eba64910ba0fa67bf04505ba956fd7f4729a
LimMinKyo <story1400@naver.com>
2021-12-20 17:39:29 +0900

    Update Movie.js and package.json

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ function Movie({id, coverImg, title, summary, genres}){
     <div>
       <img src={coverImg} alt={title}/>
       <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-      <p>{summary}</p>
+      <p>{summary.length < 255 ? `${summary.slice(0, 255)}...` : summary}</p>
       <ul>
       {genres.map(g => (
         <li key={g}>{g}</li>
