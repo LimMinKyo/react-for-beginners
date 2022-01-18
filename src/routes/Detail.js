1a122a3f02dda4cf451a073ceb989fc4488b3fc2
LimMinKyo <story1400@naver.com>
2022-01-18 10:39:51 +0900

    Apply CSS

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,31 +1,37 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Movie from "../components/Movie";
-function Detail(){
-  const {id} = useParams();
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Movie from '../components/Movie';
+import styles from './Detail.module.css';
+
+function Detail() {
+  const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [movie, setMoive] = useState([]);
-  const getMoive = async() => {
-    const json = await (await (fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`))).json();
+  const getMoive = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
     setMoive(json.data.movie);
     setLoading(false);
-  }
+  };
   useEffect(() => {
     getMoive();
   }, []);
   return (
-    <div>
-      {loading ? <h2>loading...</h2> : (
+    <div className={styles.detail}>
+      {loading ? (
+        <h2>loading...</h2>
+      ) : (
         <Movie
           id={movie.id}
-          coverImg={movie.medium_cover_image} 
-          title={movie.title} 
-          summary={movie.description_full} 
+          coverImg={movie.medium_cover_image}
+          title={movie.title}
+          summary={movie.description_full}
           genres={movie.genres}
         />
       )}
     </div>
-  )
+  );
 }
 
 export default Detail;
